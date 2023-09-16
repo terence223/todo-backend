b2696b941af296ad9cffe68680b3801db4ec1bf2
Terence <>
2023-09-17 01:17:00 +0900

    fix : fix setting bug

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
 const authRouter = require('./routes/auth');
+const todoRouter = require('./routes/todo');
 
 const app = express();
 
-app.use('/', authRouter);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -22,5 +17,14 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use('/todo', todoRouter);
+app.use('/', authRouter);
 
 module.exports = app;
